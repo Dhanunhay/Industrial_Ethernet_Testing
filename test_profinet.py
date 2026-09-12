4f3b54c9e80c84144e78b833f9c39c381e5a70cf
dhanunhay <karnamdhanunjay@gmail.com>
2026-09-12 17:03:36 +0530

    Added PROFINET connection tests

---
+++ test_profinet.py
@@ -0,0 +1,10 @@
+def test_profinet_connection():
+    controller_connected = True
+
+    assert controller_connected is True
+
+
+def test_profinet_data_exchange():
+    cyclic_data_received = True
+
+    assert cyclic_data_received is True
